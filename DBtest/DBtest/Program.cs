5353b17cfdffbfc2f5d90d15003fa388583d91e3
Vx <veikovx@gmail.com>
2012-11-07 19:39:03 +0200

    DBtest loodud

---
+++ DBtest/DBtest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
